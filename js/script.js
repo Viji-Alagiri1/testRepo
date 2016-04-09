ec305c010dc4ed76ded2fb88b837c9007de738d3
Viji-Alagiri1 <va663@nyu.edu>
2016-04-08 21:56:19 -0400

    ok

--- js/script.js
+++ js/script.js
@@ -1,21 +1,20 @@
 var basemapUrl = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
 var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
-var layerUrl = 'https://va663.cartodb.com/api/v2/viz/adfb27da-fdba-11e5-8229-0e3ff518bd15/viz.json';
+var layerUrl = 'http://va663.cartodb.com/api/v2/viz/adfb27da-fdba-11e5-8229-0e3ff518bd15/viz.json';
 
 
- //initialize map2
-  var map2 = L.map('map2', {
+ //initialize map
+  var map = new L.map('map', {
     scrollWheelZoom: false
   }).setView( [40.730982,-73.997340], 10);
 
-  
- //CartoDB Basemap
+  //CartoDB Basemap
   L.tileLayer(basemapUrl,{
     attribution: attribution
-  }).addTo(map2);
+  }).addTo(map);
 
-   cartodb.createLayer(map2, layerUrl)
-      .addTo(map2)
+   cartodb.createLayer(map, layerUrl)
+      .addTo(map)
       .on('done', function(layer) {
       }).on('error', function() {
         //log the error
