04371e4d73c6695fc5b2cefff3f77fc43437e35f
Viji-Alagiri1 <va663@nyu.edu>
2016-04-08 21:58:18 -0400

    ok

--- js/script.js
+++ js/script.js
@@ -1,22 +1,17 @@
-var basemapUrl = 'http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png';
-var attribution = '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>';
-var layerUrl = 'http://va663.cartodb.com/api/v2/viz/adfb27da-fdba-11e5-8229-0e3ff518bd15/viz.json';
-
-
- //initialize map
-  var map = new L.map('map', {
+var map = new L.Map('map', {
     scrollWheelZoom: false
   }).setView( [40.730982,-73.997340], 10);
 
-  //CartoDB Basemap
-  L.tileLayer(basemapUrl,{
-    attribution: attribution
-  }).addTo(map);
 
-   cartodb.createLayer(map, layerUrl)
+    L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
+      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
+    }).addTo(map);
+
+    var layerUrl = 'http://va663.cartodb.com/api/v2/viz/adfb27da-fdba-11e5-8229-0e3ff518bd15/viz.json';
+
+    cartodb.createLayer(map, layerUrl)
       .addTo(map)
       .on('done', function(layer) {
       }).on('error', function() {
         //log the error
       });
-
