270c3d85aab6b6aed0363eca96094a9a04b01024
Viji-Alagiri1 <va663@nyu.edu>
2016-04-09 12:41:58 -0400

    ok

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 var map = new L.Map('map', {
     scrollWheelZoom: false
-  }).setView( [40.730982,-73.997340], 10);
+  }).setView( [40.730982,-73.997340], 11);
 
     L.tileLayer('http://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}.png',{
      attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, &copy; <a href="http://cartodb.com/attributions">CartoDB</a>'
@@ -10,6 +10,7 @@ var map = new L.Map('map', {
 
 //https://va663.cartodb.com/api/v2/viz/adfb27da-fdba-11e5-8229-0e3ff518bd15/viz.json
    
+   //code from dmiketa/final (http://github.com/dmiketa/final)
   cartodb.createLayer(map, layerURL)
       .addTo(map)
       .on('done', function(layer) {
